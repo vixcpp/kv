265b6e5090b52863fb64beb2a13e15962d1f293e
Gaspard Kirira <gaspardkirira9@gmail.com>
2026-05-08 20:00:58 +0300

    align kv cmake packaging with vix modules

---
+++ cmake/vix-kv-options.cmake
@@ -0,0 +1,15 @@
+#  @file vix-kv-options.cmake
+#  @author Gaspard Kirira
+#
+#  Copyright 2026, Gaspard Kirira. All rights reserved.
+#  https://github.com/vixcpp/vix
+#  Use of this source code is governed by a MIT license
+#  that can be found in the License file.
+#
+# ====================================================================
+# Vix.cpp - KV Module Options
+# ====================================================================
+
+option(VIX_KV_BUILD_EXAMPLES "Build KV examples" OFF)
+option(VIX_KV_BUILD_TESTS "Build KV tests" OFF)
+option(VIX_KV_BUILD_BENCHMARKS "Build KV benchmarks" OFF)
